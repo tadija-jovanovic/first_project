ae0238ca407e79d71d9014ad145898e2b23a5cbd
Tadija Jovanovic <jovanovic2005@gmail.com>
2025-11-29 15:34:49 +0100

    hello world exe

---
+++ hello_c_project/hello.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+    printf("Hello, World!\n");
+    printf("This is first C app written by Tadija\n");
+    scanf("%*c", 1, NULL); // Wait for user input
+    return 0;
+}
